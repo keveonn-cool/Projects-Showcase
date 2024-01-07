6a8775bec09d7201c20a21464f709731d0eb9671
Gopal Sharma <124688881+keveonn-cool@users.noreply.github.com>
2024-01-06 19:48:38 -0700

    Update views.py

--- Django-projects/weatherProject/weatherApp/views.py
+++ Django-projects/weatherProject/weatherApp/views.py
@@ -9,7 +9,7 @@ def index(request):
         city = request.POST['city']
 
         source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' +
-                                        city + '&units=metric&appid=4a12cf568e90f7369d0685c2857b9f54').read()
+                                        city + '&units=metric&appid=<Add your API key here>').read()
         list_of_data = json.loads(source)
 
         data = {
